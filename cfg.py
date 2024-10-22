d75e72747e2a7c6239e88f6b84293c35eb8cc241
David Janes <davidjanes@gmail.com>
2024-10-22 19:03:36 -0400

    clean up

--- cfg.py
+++ cfg.py
@@ -3,7 +3,7 @@ from util import generate_dates
 
 START_DATE = "2024-11-08"
 END_DATE = "2025-02-28"
-HOLIDAYS = ["2024-12-25", "2024-12-26", "2025-01-01", "2025-02-17" ]  # Example holidays
+HOLIDAYS = ["2024-12-25", "2024-12-26", "2025-01-01", "2025-02-17" ]
 
 
 employees = [
